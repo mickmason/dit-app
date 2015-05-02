2a04447173417b5db5fcbbb99dff4b054b3a2b05
mickmason <mmason2@hotmail.com>
2015-05-02 19:03:07 +0100

    first commit, setting up the project

---
+++ data_endpoint/db_config.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "root"); // db user
+
+define('DB_PASSWORD', "charliebaker"); // db password (mention your db password here)
+define('DB_DATABASE', "collegeData"); // database name
+define('DB_SERVER', "127.0.0.1"); // db server
+?>
